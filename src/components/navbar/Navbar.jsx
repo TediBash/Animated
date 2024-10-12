d37d3b76ca611a6987826e4dfde54d4f2be41b61
TediBash <tedi.bashuri@gmail.com>
2024-10-12 13:39:15 +0200

    add config

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                     animate={{opacity:1, scale:1}}
                     transition={{duration:0.5}}
                 >
-                    Tedi Dev
+                    Tedi Dev1
                 </motion.span>
                 <div className='social'>
                     <a href='#'>
